cff77bedd2197174c96431b7c463af366a017e32
Trevor Aaron Robert Dcosta <63400592+dcostat04@users.noreply.github.com>
2021-08-05 11:47:21 +0530

    Update login.js

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,8 +1,3 @@
-import { Button,Flex,Heading,Input,ButtonGroup,Text } from "@chakra-ui/react";
-import Head from "next/head";
-import Layout from "../../components/Layout";
-
-
 export default function login() {
   return (
     <>
@@ -16,9 +11,7 @@ export default function login() {
            <Input placeholder="Roll No" variant="filled"mb={10} type="number"/>
            <Input placeholder="*******" variant="filled"mb={10} type="password"/>
            <Button colorScheme="green">Log IN</Button>
-           <Text mb={6}>For non FCRIT Students please contact:</Text>
-           <Text>Celine Fernandes:</Text>
-           <Text>Ritwik Saraf:</Text>
+           
          </Flex>
       </Flex> 
 
