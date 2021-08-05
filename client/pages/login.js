5c5960962a5b19890ff7cb2bc5cd6c423309c06a
Trevor Aaron Robert Dcosta <63400592+dcostat04@users.noreply.github.com>
2021-08-05 09:38:54 +0530

    Update login.js

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,21 +1,21 @@
 import { Button,Flex,Heading,Input,ButtonGroup,Text } from "@chakra-ui/react";
 import Head from "next/head";
-import Layout from "../components/Layout";
+import Layout from "../../components/Layout";
 
 
-export default function Home() {
+export default function login() {
   return (
     <>
       <Head>
         <title>FACES-21</title>
       </Head>
      
-     <Flex height="100vh"alignItems="center" justifyContent="center" >
-         <Flex direction="column" background="gray.100" p={12} rounded={6}>
+     <Flex height="100vh"background="green.100"alignItems="center" justifyContent="center" >
+         <Flex direction="column" background="green.200" p={12} rounded={6}>
            <Heading mb={6}>Log In</Heading>
            <Input placeholder="Roll No" variant="filled"mb={10} type="number"/>
            <Input placeholder="*******" variant="filled"mb={10} type="password"/>
-           <Button colorScheme="teal">Log IN</Button>
+           <Button colorScheme="green">Log IN</Button>
            <Text mb={6}>For non FCRIT Students please contact:</Text>
            <Text>Celine Fernandes:</Text>
            <Text>Ritwik Saraf:</Text>
