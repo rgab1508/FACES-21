ef1e4e3b261bef89bda235e187dd3f08c17f5b4b
Gabriel <rgabriel1508@gmail.com>
2021-08-08 14:45:50 +0530

    Event register route working now

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -1,7 +1,8 @@
-from .views import EventDetailVIew, EventListView
+from .views import EventDetailVIew, EventListView, EventRegiterView
 from django.urls import path
 
 urlpatterns = [
     path('', EventListView.as_view()),
-    path('<str:event_code>', EventDetailVIew.as_view())
+    path('register', EventRegiterView.as_view()),
+    path('<str:event_code>', EventDetailVIew.as_view()),
 ]
