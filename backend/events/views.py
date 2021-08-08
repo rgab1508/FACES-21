c2aab0bcb3a82baf4105ab9ddf8fdfaa21a2a718
Gabriel <rgabriel1508@gmail.com>
2021-08-08 16:04:50 +0530

    minor refactor

--- backend/events/views.py
+++ backend/events/views.py
@@ -37,92 +37,91 @@ class EventRegiterView(APIView):
   def post(self, request):
     user = request.user
     event_code = request.data['event_code']
+    event = None
     print(event_code)
     try:
       event = Event.objects.get(event_code=event_code)
-      if event.seats == event.max_seats:
-        return JsonResponse({"message": "Event Doesn't have Seats Left!", "success": False}, status=400)
-      
-      if event.team_size == 1:
-        # Event is Solo Event
-        e = user.teams.filter(event=event).first()
-        if e:
-          return JsonResponse({"message": "You have Already Registered For this Event", "success": False}, status=400)
-        
-        # create a Team of One
-        t = Team()
-        t.event = event
-        t.team_name = user.name
-        t.save()
-        t.members.add(user)
-        
-        # add to moneyOwed
-        user.money_owed += event.entry_fee
-
-        # TODO update criteria
-
-        # Update Event seats
-        event.seats += 1
-
-        try:
-          t.save()
-          event.save()
-          user.save()
-          return JsonResponse({"message": "Event Registered Sucessfully!", "success": True}, status=200)
-
-        except:
-          t.delete()
-          return JsonResponse({"message": "Something Went Wrong!", "success": False}, status=400)
-
-      else:
-        # Event is Team Event
-        e = user.teams.filter(event=event).first()
-        if e:
-          return JsonResponse({"message": "You have Already Registered For this Event", "success": False}, status=400)
-        
-        team_name = request.data['team_name']
-        members = request.data["members"]
-
-
-        if event.is_team_size_strict and len(members) != event.team_size:        
-          return JsonResponse({"message": f"Event Has a Strict Team Size of {event.team_size}", "success": False}, status=400)
-
-        if len(set(members)) != len(members):
-          return JsonResponse({"message": "Team have Repeated Members, Please ensure they are Unique!", "success": False}, status=400)
-        
-        t = Team()
-        t.event = event
-        t.team_name = team_name
-        t.save()
-
-        
-        for m in members:
-          try:
-            u = User.objects.get(roll_no=m)
-            t.members.add(u)
-          except User.DoesNotExist:
-            t.delete()
-            return JsonResponse({"message": "Roll Number is Not Valid or Doesn't Exists", "success": False}, status=400)
-        
-        # add to moneyOwed
-        user.money_owed += event.entry_fee
-
-        # TODO update criteria
-
-        # Update Event seats
-        event.seats += 1
-        
-        try:
-          t.save()
-          event.save()
-          user.save()
-          return JsonResponse({"message": "Event Registered Sucessfully!", "success": True}, status=200)
-        except:
-          t.delete()
-          return JsonResponse({"message": "Something Went Wrong!", "success": False}, status=400)
-        
-
-
     except Event.DoesNotExist:
       return JsonResponse({"message": "Event Doesn't Exists", "success": False}, status=400)
+
+
+    if event.seats == event.max_seats:
+      return JsonResponse({"message": "Event Doesn't have Seats Left!", "success": False}, status=400)
     
+    if event.team_size == 1:
+      # Event is Solo Event
+      e = user.teams.filter(event=event).first()
+      if e:
+        return JsonResponse({"message": "You have Already Registered For this Event", "success": False}, status=400)
+      
+      # create a Team of One
+      t = Team()
+      t.event = event
+      t.team_name = user.name
+      t.save()
+      t.members.add(user)
+      
+      # add to moneyOwed
+      user.money_owed += event.entry_fee
+
+      # TODO update criteria
+
+      # Update Event seats
+      event.seats += 1
+
+      try:
+        t.save()
+        event.save()
+        user.save()
+        return JsonResponse({"message": "Event Registered Sucessfully!", "success": True}, status=200)
+
+      except:
+        t.delete()
+        return JsonResponse({"message": "Something Went Wrong!", "success": False}, status=400)
+
+    else:
+      # Event is Team Event
+      e = user.teams.filter(event=event).first()
+      if e:
+        return JsonResponse({"message": "You have Already Registered For this Event", "success": False}, status=400)
+      
+      team_name = request.data['team_name']
+      members = request.data["members"]
+
+
+      if event.is_team_size_strict and len(members) != event.team_size:        
+        return JsonResponse({"message": f"Event Has a Strict Team Size of {event.team_size}", "success": False}, status=400)
+
+      if len(set(members)) != len(members):
+        return JsonResponse({"message": "Team have Repeated Members, Please ensure they are Unique!", "success": False}, status=400)
+      
+      t = Team()
+      t.event = event
+      t.team_name = team_name
+      t.save()
+
+      
+      for m in members:
+        try:
+          u = User.objects.get(roll_no=m)
+          t.members.add(u)
+          # TODO update criteria for members
+
+        except User.DoesNotExist:
+          t.delete()
+          return JsonResponse({"message": "Roll Number is Not Valid or Doesn't Exists", "success": False}, status=400)
+      
+      # add to moneyOwed
+      user.money_owed += event.entry_fee
+
+      # Update Event seats
+      event.seats += 1
+      
+      try:
+        t.save()
+        event.save()
+        user.save()
+        return JsonResponse({"message": "Event Registered Sucessfully!", "success": True}, status=200)
+      except:
+        t.delete()
+        return JsonResponse({"message": "Something Went Wrong!", "success": False}, status=400)
