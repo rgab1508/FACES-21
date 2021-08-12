99c81c1fb74a5eb147e0ab0ce9c0daefb1652312
Gabriel <rgabriel1508@gmail.com>
2021-08-13 01:38:01 +0530

    added transaction admin

--- backend/transactions/admin.py
+++ backend/transactions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+  filter_fields = ('verified')
+  search_fields = ('transaction_id', 'transaction_code')
