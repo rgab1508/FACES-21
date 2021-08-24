f8aba872379622a95bc9fb460b58ca73ef1a56f7
Gabriel <rgabriel1508@gmail.com>
2021-08-24 15:19:31 +0530

    minor changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -172,7 +172,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
